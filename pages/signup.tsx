5747c17d5fc017f1fa95c9b194fc8c5b8732f073
SayedaRehmat <bc190408541@vu.edu.pk>
2025-10-18 13:27:28 +0500

    Fix import path and update AuthForm prop name

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,4 +1,5 @@
-import AuthForm from "@/components/AuthForm";
+import AuthForm from "../components/AuthForm";
+
 export default function SignupPage() {
-  return <AuthForm type="signup" />;
+  return <AuthForm mode="signup" />;
 }
